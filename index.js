0e4c5aac1c0f35d8fac3cffcc8ed276498995c80
thayari <alex.silvanovich@gmail.com>
2021-04-18 13:54:23 +0300

    update team

--- index.js
+++ index.js
@@ -1,89 +1,120 @@
 const team = [
   {
-    name: 'Дарья Малинина',
-    img: './img/team/malinina_darya.jpg',
-    info: '',
-  },
-  {
     name: 'Кирилл Решетников',
     img: './img/team/reshetnikov_kirill.jpg',
-    info: '',
+    info: 'Team-leader',
   },
-  {
-    name: 'Алена Старикова',
-    img: './img/team/starikova_alyona.jpg',
-    info: '',
-  },
+
   {
     name: 'Вера Сысоева',
     img: './img/team/sysoeva_vera.jpg',
-    info: '',
+    info: 'Team-leader',
   },
+
   {
-    name: 'Марина Фроленкова',
-    img: './img/team/frolenkova_marina.jpg',
-    info: '',
+    name: 'Дарья Малинина',
+    img: './img/team/malinina_darya.jpg',
+    info: 'Куратор научной части',
   },
+
   {
-    name: 'Анастасия Кислова',
-    img: './img/team/kislova_anastasia.jpg',
-    info: '',
-  },
-  {
-    name: 'Полина Журлова',
-    img: './img/team/zhurlova_polina.jpg',
-    info: '',
-  },
-  {
-    name: 'Александр Минаков',
-    img: './img/team/minakov_alexander.jpg',
-    info: '',
-  },
-  {
-    name: 'Юлия Васильева',
-    img: './img/team/vasilyeva_julia.jpg',
-    info: '',
+    name: 'Алена Старикова',
+    img: './img/team/starikova_alyona.jpg',
+    info: 'Куратор научной части',
   },
   {
     name: 'Валерия Спиридонова',
     img: './img/team/spiridonova_valeria.jpg',
-    info: '',
+    info: 'Ответственная за коллаборации',
   },
+
   {
-    name: 'Анна Капитонова',
-    img: './img/team/kapitonova_anna.jpg',
-    info: '',
+    name: 'Полина Журлова',
+    img: './img/team/zhurlova_polina.jpg',
+    info: 'Ответственная за SMM',
   },
-  {
-    name: 'Надежда Азбукина',
-    img: './img/team/azbukina_nadezhda.jpg',
-    info: '',
-  },
-  {
-    name: 'Вячеслав Паско',
-    img: './img/team/pasko_vyacheslav.jpg',
-    info: '',
-  },
-  {
-    name: 'Диана Загирова',
-    img: './img/team/zagirova_diana.jpg',
-    info: '',
-  },
-  {
-    name: 'Екатерина Колодяжная',
-    img: './img/team/kolodyazhnaya_ekaterina.jpg',
-    info: '',
-  },
+
   {
     name: 'Виктория Чебаненко',
     img: './img/team/chebanenko_viktorya.jpg',
-    info: '',
+    info: 'Ответственная за PR',
   },
+
+  {
+    name: 'Марина Фроленкова',
+    img: './img/team/frolenkova_marina.jpg',
+    info: 'Ответственная за фандрайзинг',
+  },
+
+  {
+    name: 'Анастасия Кислова',
+    img: './img/team/kislova_anastasia.jpg',
+    info: 'Ответственная за мероприятия',
+  },
+
+
+  {
+    name: 'Вячеслав Паско',
+    img: './img/team/pasko_vyacheslav.jpg',
+    info: 'Сухая лаборатория',
+  },
+
+  {
+    name: 'Валентина Костенко',
+    img: './img/team/kostenko_valentina.jpg',
+    info: 'Связи с общественностью',
+  },
+
+
+  {
+    name: 'Надежда Азбукина',
+    img: './img/team/azbukina_nadezhda.jpg',
+    info: 'Сухая лаборатория',
+  },
+
+  {
+    name: 'Екатерина Колодяжная',
+    img: './img/team/kolodyazhnaya_ekaterina.jpg',
+    info: 'Мокрая лаборатория',
+  },
+
+
+  {
+    name: 'Анна Капитонова',
+    img: './img/team/kapitonova_anna.jpg',
+    info: 'Связи с общественностью',
+  },
+
+  {
+    name: 'Юлия Васильева',
+    img: './img/team/vasilyeva_julia.jpg',
+    info: 'Связи с общественностью',
+  },
+
+  {
+    name: 'Константин Каменский',
+    img: './img/team/kamenskii_konstantin.jpg',
+    info: 'Связи с общественностью',
+  },
+
+  {
+    name: 'Диана Загирова',
+    img: './img/team/zagirova_diana.jpg',
+    info: 'Связи с общественностью',
+  },
+
   {
     name: 'Екатерина Ганжула',
     img: './img/team/ganzhula_ekaterina.jpg',
-    info: '',
+    info: 'Связи с общественностью',
   },
+
+ {
+    name: 'Александр Минаков',
+    img: './img/team/minakov_alexander.jpg',
+    info: 'Сухая лаборатория',
+  },
+
   {
     name: 'Дарья Лунегова',
     img: './img/team/lunegova_darya.jpg',
@@ -92,52 +123,61 @@ const team = [
   {
     name: 'Екатерина Кравчук',
     img: './img/team/kravchuk_ekaterina.jpg',
-    info: '',
+    info: 'Сухая лаборатория',
   },
   {
     name: 'Анастасия Щербакова',
     img: './img/team/sherbakova_anastasia.jpg',
-    info: '',
+    info: 'Связи с общественностью',
   },
   {
     name: 'Ксения Кусакина',
     img: './img/team/kusacina_ksenia.jpg',
-    info: '',
+    info: 'Мокрая лаборатория',
   },
   {
     name: 'Дарья Баранова',
     img: './img/team/baranova_darya.jpg',
-    info: '',
+    info: 'Связи с общественностью',
   },
   {
     name: 'Наида Мамаева',
     img: './img/team/mamaeva_naida.jpg',
-    info: '',
+    info: 'Мокрая лаборатория',
   },
   {
     name: 'Георгий Чистов',
     img: './img/team/chistov_george.jpg',
-    info: '',
+    info: 'Сухая лаборатория',
   },
   {
     name: 'Павел Кравченко',
     img: './img/team/kravchenko_pavel.jpg',
-    info: '',
+    info: 'Сухая лаборатория',
   },
-  {
-    name: 'Валентина Костенко',
-    img: './img/team/kostenko_valentina.jpg',
-    info: '',
-  }, 
+ 
   {
     name: 'Никита Моторин',
     img: './img/team/motorin_nikita.jpg',
-    info: '',
+    info: 'Сухая лаборатория',
   },
+
   {
     name: 'Маргарита Егиян',
     img: './img/team/egian_margarita.jpg',
-    info: '',
+    info: 'Связи с общественностью',
+  },
+
+  {
+    name: 'Никита Моторин',
+    img: './img/team/motorin_nikita.jpg',
+    info: 'Сухая лаборатория',
+  },
+
+  {
+    name: 'Артём Мурзин',
+    img: './img/team/murzin_artyom.jpg',
+    info: 'Сухая лаборатория',
   },
 ];
 
@@ -148,6 +188,7 @@ function renderTeam (list) {
     html += `<div class="team__item">
         <div class="team__portrait" style="background-image: url(${member.img});"></div>
         <h3 class="team__name">${member.name}</h3>
+        <p class="team__info">${member.info}</p>
       </div>`;
   });
   itemsWrapper.innerHTML = html;
